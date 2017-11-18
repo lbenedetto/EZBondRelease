2f57b4e9a924747deb548bf749902b20950aca76
lbenedetto <larsbenedetto@gmail.com>
2017-11-17 17:24:19 -0800

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,159 @@
+import javax.swing.*;
+import javax.swing.text.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Main extends JFrame {
+	private JPanel contentPane;
+	private JTextPane textPane;
+	private JTextField textField;
+	private Document doc;
+	private AttributeSet asWhite;
+	private AttributeSet asOffWhite;
+	private BufferedWriter bw;
+	private static HashMap<String, ArrayList<Vehicle>> lotus = new HashMap<>();
+
+	private Main() throws IOException {
+		setContentPane(contentPane);
+
+		// call onCancel() when cross is clicked
+		setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+		addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				onCancel();
+			}
+		});
+
+		// call onCancel() on ESCAPE
+		contentPane.registerKeyboardAction(e -> onCancel(),
+				KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0),
+				JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+
+		textField.addActionListener(e -> process());
+		doc = textPane.getDocument();
+		textPane.setEditable(false);
+		StyleContext sc = StyleContext.getDefaultStyleContext();
+		//Color 1 - White
+		asWhite = sc.addAttribute(SimpleAttributeSet.EMPTY, StyleConstants.Foreground, new Color(255, 255, 255));
+		asWhite = sc.addAttribute(asWhite, StyleConstants.FontFamily, "Lucida Console");
+		asWhite = sc.addAttribute(asWhite, StyleConstants.Alignment, StyleConstants.ALIGN_JUSTIFIED);
+		sc = StyleContext.getDefaultStyleContext();
+		//Color 2 - Off White
+		asOffWhite = sc.addAttribute(SimpleAttributeSet.EMPTY, StyleConstants.Foreground, new Color(200, 200, 200));
+		asOffWhite = sc.addAttribute(asOffWhite, StyleConstants.FontFamily, "Lucida Console");
+		asOffWhite = sc.addAttribute(asOffWhite, StyleConstants.Alignment, StyleConstants.ALIGN_JUSTIFIED);
+		//File file = new File(new Timestamp(System.currentTimeMillis()).toString() + ".txt");
+		String time = new Timestamp(System.currentTimeMillis()).toString() + ".txt";
+		File file = new File(time.replaceAll(":", ""));
+		if (file.createNewFile())
+			bw = new BufferedWriter(new FileWriter(file));
+		loadLotus();
+		textPane.setBackground(new Color(43, 43, 43));
+		log(String.format("Saving to %s", time), false);
+	}
+
+	private void process() {
+		String stockNumber = textField.getText().toUpperCase().trim();
+		if (stockNumber.startsWith("MANUAL")) {
+			String[] d = stockNumber.split(" ");
+			save(String.format("%s,%s,%s", d[1], d[2], d[3]));
+		} else if (stockNumber.length() >= 8) {
+			handle(stockNumber, 8);
+		} else if (stockNumber.length() >= 6) {
+			handle(stockNumber, 6);
+		} else {
+			log("Invalid length", true);
+		}
+	}
+
+	private void handle(String stockNumber, int length) {
+		stockNumber = stockNumber.substring(stockNumber.length() - length, stockNumber.length());
+		ArrayList<Vehicle> av = lotus.get(stockNumber);
+		if (av != null && av.size() == 1) {
+			Vehicle v = av.get(0);
+			if (v.validUPS()) {
+				save(v.toString());
+				textField.setText("");
+				return;
+			}
+			log("Invalid UPS number in Lotus\n" +
+					"RECOMMENDED: Enter the information manually with the format \"MANUAL VIN ENTRY UPS\"", true);
+			return;
+		}
+		log("Either none or multiple entries found!\n" +
+				"RECOMMENDED: Enter the information manually with the format \"MANUAL VIN ENTRY UPS\"", true);
+		if (length == 6) {
+			log("Or, try again with the last 8 instead of the last 6", true);
+		}
+	}
+
+	private static void loadLotus() {
+		try {
+			Files.lines(Paths.get("U:\\Filing\\Bond Release Database.txt")).forEach(data -> {
+				String[] datum = data.split(",");
+				String vin = datum[0].trim();
+				Vehicle vehicle = new Vehicle(data);
+				if (vin.length() > 6) {
+					if (vin.length() > 8) {
+						addToLotus(vehicle, vin.substring(vin.length() - 8, vin.length()));
+					}
+					addToLotus(vehicle, vin.substring(vin.length() - 6, vin.length()));
+				} else {
+					addToLotus(vehicle, vin);
+				}
+
+			});
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void addToLotus(Vehicle vehicle, String stock) {
+		lotus.putIfAbsent(stock, new ArrayList<>());
+		lotus.get(stock).add(vehicle);
+	}
+
+	private void log(String msg, boolean isWhite) {
+		int len = textPane.getDocument().getLength();
+		try {
+			textPane.getDocument().insertString(len, msg + "\n", isWhite ? asWhite : asOffWhite);
+		} catch (BadLocationException e) {
+			e.printStackTrace();
+		}
+		System.out.println(msg);
+	}
+
+	private void save(String msg) {
+		log(msg, false);
+		try {
+			bw.write(msg + "\r\n");
+			bw.flush();
+		} catch (IOException e) {
+			log("Could not save output!!!!!", true);
+		}
+	}
+
+	private void onCancel() {
+		// add your code here if necessary
+		dispose();
+	}
+
+	public static void main(String[] args) {
+		try {
+			Main window = new Main();
+			window.setSize(720, 480);
+			window.setVisible(true);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/Vehicle.java
@@ -0,0 +1,90 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Vehicle {
+	private String VIN, entryDate, ups;
+	private static final Pattern threeComma = Pattern.compile("(^[^,]*,[^,]*,[^,]*)");
+	private static final Pattern dataPattern = Pattern.compile("(^[^,]+)?,([^,]*)?,([^,]*)?$");
+	private static final Pattern UPS = Pattern.compile("^(\\w{3}|\\d{3})-\\d{7}-\\d$");
+	private static final int FIELDS = 3;
+
+	Vehicle(String data) {
+		data = simplify(data);
+		String[] datum = data.split(",");
+		if (datum[0].equals("VIN NUMBER")) return;
+		if (datum.length != FIELDS) {
+			//The formatting of the file is broken, but we can't risk losing any data
+			Matcher m = dataPattern.matcher(data);
+			datum = new String[FIELDS];
+			if (m.find()) {
+				for (int i = 0; i < m.groupCount(); i++) {
+					datum[i] = m.group(i + 1);
+				}
+			}
+		}
+		this.VIN = clean(datum[0]);
+		this.entryDate = clean(datum[1]);
+		this.ups = clean(datum[2]);
+	}
+
+	boolean validUPS() {
+		return UPS.matcher(ups).find();
+	}
+
+	private static String clean(String s) {
+		return fixEmpty(s).toUpperCase().trim();
+	}
+
+	private static String fixEmpty(String s) {
+		return s == null || s.isEmpty() ? "???" : s;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("%s,%s,%s", VIN, entryDate, ups);
+	}
+
+	@Override
+	public int hashCode() {
+		return this.VIN.hashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof Vehicle) {
+			Vehicle v = (Vehicle) obj;
+			if (VIN.equals(v.VIN) &&
+					equalsIgnoreUnknown(entryDate, v.entryDate) &&
+					equalsIgnoreUnknown(ups, v.ups)) {
+				//Merge wildcard data
+				if (!entryDate.equals("???") ^ !v.entryDate.equals("???")) {
+					entryDate = entryDate.equals("???") ? v.entryDate : entryDate;
+					v.entryDate = v.entryDate.equals("???") ? entryDate : v.entryDate;
+				}
+				if (!ups.equals("???") ^ !v.ups.equals("???")) {
+					ups = ups.equals("???") ? v.ups : ups;
+					v.ups = v.ups.equals("???") ? ups : v.ups;
+				}
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private static boolean equalsIgnoreUnknown(String s1, String s2) {
+		//They are the same if either one is a wildcard
+		return s1.equals("???") || s2.equals("???") || s1.equals(s2);
+	}
+
+	private static String simplify(String s) {
+		s = s.toUpperCase()
+				.replace("\"", "")
+				.replaceAll("\t", "")
+				.trim();
+		Matcher m = threeComma.matcher(s);
+		if (m.find()) {
+			s = m.group(1);
+		}
+		return s;
+	}
+}
